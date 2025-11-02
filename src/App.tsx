3d381c93cb4621a96d8f6daa31f70b1209a09767
Sharad Aghara <sharadaghara24@gmail.com>
2025-11-02 23:16:26 +0530

    feat: wrap Layout component with DataProvider for context access

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import { DataProvider } from "./context/DataProvider";
 import { Layout } from "./layout/Layout";
 
 function App() {
-    return <Layout />;
+    return (
+        <DataProvider>
+            {/* Everything under data provider can use it */}
+            <Layout />
+        </DataProvider>
+    )
 }
 
 export default App;
