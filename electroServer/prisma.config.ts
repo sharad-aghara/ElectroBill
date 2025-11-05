d7f927d4e14787c0fed9146371a27c2d743de94d
Sharad Aghara <sharadaghara24@gmail.com>
2025-11-06 00:03:30 +0530

    feat: configured prisma

--- electroServer/prisma.config.ts
+++ electroServer/prisma.config.ts
@@ -1,13 +1,13 @@
-import "dotenv/config";
+// prisma.config.ts
+import dotenv from "dotenv";
+import path from "path";
 import { defineConfig, env } from "prisma/config";
+
+dotenv.config({ path: path.resolve(process.cwd(), "prisma", ".env") });
 
 export default defineConfig({
     schema: "prisma/schema.prisma",
-    migrations: {
-        path: "prisma/migrations",
-    },
+    migrations: { path: "prisma/migrations" },
     engine: "classic",
-    datasource: {
-        url: env("DATABASE_URL"),
-    },
+    datasource: { url: env("DATABASE_URL") },
 });
