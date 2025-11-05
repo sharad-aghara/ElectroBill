6d8248d9e61cc93960cd71ccefe2d5258e692e36
Sharad Aghara <sharadaghara24@gmail.com>
2025-11-06 00:09:25 +0530

    prisma: This implement prevents multiple clients being created during hot reload / reimport, which can exhaust DB connections.

---
+++ electroServer/src/prisma/client.ts
@@ -0,0 +1,17 @@
+// prevents multiple clients being created during hot reload / reimport, which can exhaust DB connections.
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // avoid creating multiple clients in dev with HMR
+  // eslint-disable-next-line no-var
+  var __prisma?: PrismaClient;
+}
+
+export const prisma = 
+    global.__prisma ??
+    new PrismaClient({
+        log: process.env.NODE_ENV === "development" ? ['query', 'info'] : [];
+    });
+
+    if(process.env.NODE_ENV === "development") global.__prisma = prisma;
