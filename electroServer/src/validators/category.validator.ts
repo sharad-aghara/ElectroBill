53d266e7283cf28394d8f90e120a271310484232
Sharad Aghara <sharadaghara24@gmail.com>
2025-11-07 00:41:19 +0530

    update: updated category validator to validate update and remove parameters

--- electroServer/src/validators/category.validator.ts
+++ electroServer/src/validators/category.validator.ts
@@ -4,3 +4,13 @@ export const createCategorySchema = z.object({
     name: z.string().min(3),
     active: z.boolean().optional(),
 });
+
+export const updateCategorySchema = z.object({
+    id: z.string(),
+    name: z.string().min(3),
+    active: z.boolean().optional(),
+});
+
+export const deleteCategorySchema = z.object({
+    id: z.string()
+});
