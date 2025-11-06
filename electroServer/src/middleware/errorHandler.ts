14fb21bbab2ede957c604b95f6bcb2d83078e0b7
Sharad Aghara <sharadaghara24@gmail.com>
2025-11-07 00:40:33 +0530

    update: updated errorHandler to handle P2025 error code for object not found

--- electroServer/src/middleware/errorHandler.ts
+++ electroServer/src/middleware/errorHandler.ts
@@ -11,7 +11,13 @@ export default function errorHandler(
     if (err?.code === "P2002") {
         return res
             .status(400)
-            .json({ error: "Unoque constraint failed", meta: err.meta });
+            .json({ error: "Unique constraint failed", meta: err.meta });
+    }
+
+    if (err?.code === "P2025") {
+        return res
+            .status(400)
+            .json({ error: "Object not found", meta: err.meta });
     }
 
     res.status(err?.status || 500).json({
