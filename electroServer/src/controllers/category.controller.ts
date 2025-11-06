da1b4ee9c11ed79a970f876ca69cf62f216769fc
Sharad Aghara <sharadaghara24@gmail.com>
2025-11-07 00:37:28 +0530

    refact: refactored category controller with updated implementation of category server for update and remove categories

--- electroServer/src/controllers/category.controller.ts
+++ electroServer/src/controllers/category.controller.ts
@@ -1,40 +1,49 @@
 import type { NextFunction, Request, Response } from "express";
 import categoryService from "../services/category.service.js";
+import type { Category } from "@prisma/client";
 
 const list = async (_req: Request, res: Response, next: NextFunction) => {
     try {
+        // console.log("Category controller list initialized");
         const items = await categoryService.list();
         res.json(items);
+    } catch (err) {
+        // console.log("Category controller err: ", err);
+        next(err);
+    }
+};
+
+const create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const created = await categoryService.create(req.body);
+        res.status(201).json(created);
     } catch (err) {
         next(err);
     }
 };
 
-const create = async(req:Request, res:Response, next: NextFunction) => {
+const update = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const created = await categoryService.create(req.body);
-        res.status(201).json(created);
-    } catch(err) {
-        next(err);
-    }
-}
+        const id: string = req.params.id as string;
+        const data: Category = req.body;
 
-const update = async(req:Request, res:Response, next: NextFunction) => {
-    try {
-        const created = await categoryService.update(req.body);
-        res.status(201).json(created);
-    } catch(err) {
+        const updated = await categoryService.update(id, data);
+        res.status(201).json(updated);
+    } catch (err) {
         next(err);
     }
-}
+};
 
-const remove = async(req:Request, res:Response, next: NextFunction) => {
+const remove = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const created = await categoryService.remove(req.body);
-        res.status(201).json(created);
-    } catch(err) {
+        const id: string = req.params.id as string;
+        // console.log("id: ", id);
+
+        const removed = await categoryService.remove(id);
+        res.status(201).json(removed);
+    } catch (err) {
         next(err);
     }
-}
+};
 
 export default { list, create, update, remove };
